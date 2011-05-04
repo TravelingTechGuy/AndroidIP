d391bbe45f8325345ff5e53da33d8e0e258ed006
Guy Vider <guy@guyvider.com>
2011-05-04 13:15:12 -0700

    Initial Commit

---
+++ src/com/ttg/androidip/App.java
@@ -0,0 +1,14 @@
+package com.ttg.androidip;
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class App extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
